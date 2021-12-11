b201ec680ee697df81e444b354f33c8a6ade7b0d
parappa20 <88298540+james182-dang@users.noreply.github.com>
2021-12-11 16:50:32 -0600

    finish user-controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -57,7 +57,20 @@ const userController = {
               res.json(dbUserData);
           })
           .catch(err => res.status(400).json(err));
+    },
+
+    // delete user by id
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+          .then(dbUserData => {
+              if (!dbUserData) {
+                  res.status(404).json({ message: 'No user found with that id. '});
+                  return;
+              }
+              res.json(dbUserData);
+          })
+          .catch(err => res.status(400).json(err));
     }
-}
+};
 
 module.exports = userController;
