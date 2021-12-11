8d6e757f398743565428e97071ec3c7a7a25aaf6
parappa20 <88298540+james182-dang@users.noreply.github.com>
2021-12-11 14:30:41 -0600

    set up User model

---
+++ models/User.js
@@ -0,0 +1,16 @@
+const { Schema, model, Types } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: 'Please input a username for your profile.',
+        trim: true,
+        unique
+    },
+    email: {
+        type: String,
+        required: 'Please enter a valid email address.',
+        unique,
+        match: [/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/]
+    }
+})
