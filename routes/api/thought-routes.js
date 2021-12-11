f00907aea4d90b81e92bda03a120e6bbba603aab
parappa20 <88298540+james182-dang@users.noreply.github.com>
2021-12-11 17:11:50 -0600

    set up thought controller, begin setup thought-routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const {
+    addThought,
+    addReaction,
+    removeThought,
+    removeReaction
+} = require('../../controllers/thought-controller');
+
